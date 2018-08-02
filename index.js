a1a358f6b5ee447e967c7a5c0eb5fa01e02509f5
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-08-02 21:30:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ function destructivelyUpdateDriverWithKeyAndValue (driver, key, value) {
 
   return driver;
 }
+function deleteFromDriverByKey (driver, key) {
+  const newObj = Object.assign({}, driver);
+
+  delete newObj[key];
+
+  return newObj;
+}
